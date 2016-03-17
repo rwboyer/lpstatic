ef9f4773e5d28934ac5d26ded54520ebccda9860
Robert W Boyer <rwboyer@mac.com>
2016-03-17 10:23:17 -0400

    Form spree modal pop-up with url base…

--- js/src/app.js
+++ js/src/app.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
     $(this).addClass('active');
   });
 });
+
+function getQueryVariable(variable) {
+     var query = window.location.search.substring(1);
+     var vars = query.split("&");
+     for (var i=0;i<vars.length;i++) {
+          var pair = vars[i].split("=");
+          if(pair[0] == variable){return pair[1];}
+     }
+     return(false);
+}
+
+if (window.location.search) {
+     var showModal = getQueryVariable('showModal');
+     if (showModal == 'yes') {
+          $('#one').foundation('reveal', 'open');
+     }
+}
