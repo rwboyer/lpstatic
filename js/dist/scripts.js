5262dc6af0a38832597efa5f426079e6517a52ec
Robert W Boyer <rwboyer@mac.com>
2016-03-24 08:51:26 -0400

    darken sticky header on scroll

--- js/dist/scripts.js
+++ js/dist/scripts.js
@@ -31,10 +31,12 @@ if (window.location.search) {
 jQuery(window).scroll(function() {
     if ($(document).scrollTop() >= 50) {
         $(".important-class").addClass("padding-on-my-header");
+        $(".header-section").addClass("darken");
         $('#logo-image').attr('src', 'images/1445814383284.jpg');
     }
     else{
         $(".important-class").removeClass("padding-on-my-header");
+        $(".header-section").removeClass("darken");
         $('#logo-image').attr('src', 'images/1445814383284.jpg');
     }
 });
--- js/src/init.js
+++ js/src/init.js
@@ -1,10 +1,12 @@
 jQuery(window).scroll(function() {
     if ($(document).scrollTop() >= 50) {
         $(".important-class").addClass("padding-on-my-header");
+        $(".header-section").addClass("darken");
         $('#logo-image').attr('src', 'images/1445814383284.jpg');
     }
     else{
         $(".important-class").removeClass("padding-on-my-header");
+        $(".header-section").removeClass("darken");
         $('#logo-image').attr('src', 'images/1445814383284.jpg');
     }
 });
